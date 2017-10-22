06e32e7bd6e3756ee1172e9e322e652961ff7a60
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-10-22 08:39:27 +0200

    refactoring code

--- express/express.js
+++ express/express.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
-    res.json('ping')
+    res.json('ping');
     res.status(403).json({ error: 'Method unauthorized' });
     res.status(200).json({ error: 'Ping OK' });
 })
 
 
 app.listen(3001, function () {
-    console.log('Example app listening on port 3001!')
+    console.log('Example app listening on port 3001!');
 })
